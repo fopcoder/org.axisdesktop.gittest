b421682c94dc4aaeb72ac59f850fdc93a1403eb8
fxcoder <barabass@gmail.com>
2015-04-25 00:03:53 +0300

    new class Animal

---
+++ GitTest/src/org/axisdesktop/gittest/Animal.java
@@ -0,0 +1,32 @@
+package org.axisdesktop.gittest;
+
+public class Animal {
+	private int legs;
+	private String name;
+	private double weight;
+
+	public int getLegs() {
+		return legs;
+	}
+
+	public void setLegs( int legs ) {
+		this.legs = legs;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight( double weight ) {
+		this.weight = weight;
+	}
+
+}
